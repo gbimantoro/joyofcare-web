aac77615b484f9d1d37c464ccd75a8e1229725b8
GBimantoro <bimo@gbimantoro.com>
2026-09-05 20:19:21 +0700

    Clean: remove all secrets and node_modules from repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import keystatic from '@keystatic/astro';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   integrations: [
     mdx(),
     keystatic(),
   ],
-  output: 'static',
+  output: 'server',
+  adapter: cloudflare(),
   trailingSlash: 'always',
-  build: {
-    format: 'directory',
-  },
 });
